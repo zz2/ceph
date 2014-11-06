d610b01dfd5e4500969a3a55dbf7cdd2b17ad5be
Jason Dillaman <dillaman@redhat.com>
2014-11-06 09:59:57 -0500

    cls_rbd: Add support for image flags
    Image flags are an optional addition to the RBD image header
    which can be used to preserve state.  The flags support
    snapshot history.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/cls/rbd/cls_rbd_client.h
+++ src/cls/rbd/cls_rbd_client.h
@@ -48,6 +48,10 @@ namespace librbd {
 		   uint64_t *parent_overlap);
     int set_parent(librados::IoCtx *ioctx, const std::string &oid,
 		   parent_spec pspec, uint64_t parent_overlap);
+    int get_flags(librados::IoCtx *ioctx, const std::string &oid,
+                  snapid_t snap_id, uint64_t *flags);
+    int set_flags(librados::IoCtx *ioctx, const std::string &oid,
+                  uint64_t flags, uint64_t mask);
     int remove_parent(librados::IoCtx *ioctx, const std::string &oid);
     int add_child(librados::IoCtx *ioctx, const std::string &oid,
 		  parent_spec pspec, const std::string &c_imageid);
